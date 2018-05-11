8fcc5d19c017b69284f0bd1ad6556319f5c8ce83
Mousecop <joaskew13@gmail.com>
2018-05-11 14:36:38 -0400

    Adds header comp and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header/header';
 
 export default class App extends React.Component {
 	render() {
+		const { container, header, content } = styles;
 		return (
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-				<Text>Changes you make will automatically reload.</Text>
-				<Text>Shake your phone to open the developer menu.</Text>
+			<View style={container}>
+				<View style={header}>
+					<Header title="Awesomest App Ever!!!" />
+				</View>
+				<View style={content}>
+					<Text>Open up App.js to start working on your app!</Text>
+					<Text>Changes you make will automatically reload.</Text>
+					<Text>Shake your phone to open the developer menu.</Text>
+				</View>
 			</View>
 		);
 	}
@@ -16,7 +23,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: '#eee',
+		justifyContent: 'center'
+	},
+	header: {
+		flex: 1
+	},
+	content: {
+		flex: 6,
 		alignItems: 'center',
 		justifyContent: 'center'
 	}
